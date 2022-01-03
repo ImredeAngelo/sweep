071575275d4967e37dfd6faea67d7771971077a7
Imre Angelo <ImredeAngelo@users.noreply.github.com>
2022-01-03 14:30:21 +0100

    OpenGL

--- sweep/src/main.cpp
+++ sweep/src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "Hello World\n";
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow* window = glfwCreateWindow(1080, 720, "Sweep", nullptr, nullptr);
+
+	if (!window)
+	{
+		std::cout << "Hello World\n";
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+	int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	
+	while (!glfwWindowShouldClose(window)) {
+		
+	}
+
+	glfwTerminate();
+	return 0;
 }
